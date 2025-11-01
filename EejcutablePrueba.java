73cc551bf4e47eb76042f513fa83bbaf6b57ab45
laran <quintanarijoan@gmail.com>
2025-11-01 16:58:50 -0300

    ejemplo texto y fullscreen

--- EejcutablePrueba.java
+++ EejcutablePrueba.java
@@ -5,5 +5,10 @@ public class EejcutablePrueba
     public static void main(String []args){
         Ventana miVentana = new Ventana();
         miVentana.show();
+        VentanaConEtiquetas venta2 = new VentanaConEtiquetas();
+        venta2.show();
+        
+        EjemploTexto tex = new EjemploTexto();
+        tex.show();
     }
 }
